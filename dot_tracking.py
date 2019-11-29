bd2a890b2e191bb3be7fc42c9d15d338d7813343
devin.kaiser <devin.kaiser@thalmic.com>
2019-11-29 16:17:07 -0500

    Added speed and timestamp overlay to output video. Added position & speed calculations to output csv.

--- dot_tracking.py
+++ dot_tracking.py
@@ -2,8 +2,17 @@ import sys
 import cv2
 from datetime import datetime
 import csv
+import msvcrt
+
 colour = 'green'
+show_tracker = True
 last_position = (0, 0)
+last_time = 0
+x_scaling = 16.254
+y_scaling = 16.254
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scaling = 0.5
+font_thickness = 1
 
 # if no commandline arguments are sent, load the default video
 if len(sys.argv) == 1:
@@ -18,7 +27,7 @@ out_filename = user + ' ' + \
                datetime.strftime(datetime.now(), "%y%m%d%H%M%S") + '.mp4'
 
 f = open(csv_filename, "w+")
-f.write("Time, X, Y, Delta X, Delta Y\n")
+f.write("Time, X, Y, Delta X (px), Delta Y(px), Delta X (mmm), Delta Y (mm), Delta (mm), Speed (mm/s)\n")
 f.close()
 
 if colour == 'green':
@@ -60,6 +69,13 @@ else:
 
     # Process the video
     while(cap.isOpened()):
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            key = key.decode("utf-8")
+            if key == 'x':
+                print ("Aborted.")
+                break
+
         # capture frame and get frame time in milliseconds
         ret, frame = cap.read()
         if not ret:
@@ -93,18 +109,40 @@ else:
             position = (int(cx), int(cy))
             cv2.circle(frame_out, position, 1, (0, 0, 255), 3)
             cv2.circle(frame_out, position, int(r), (0, 0, 128), 3)
+            cv2.circle(tracker, position, 1, (0, 0, 255), 3) # add dot to center of tracked object in output video file
         else:
             position = (0, 0)   # if no contour is found
 
-        if (last_position[0] ==0 and last_position[1] == 0):
-            delta = (0,0)
+        if last_position[0] ==0 and last_position[1] == 0:
+            delta_px = (0,0)
         else:
-            delta = (position[0] - last_position[0], position[1] - last_position[1])
+            delta_px = (position[0] - last_position[0], position[1] - last_position[1])
+        delta_mm = (delta_px[0]/x_scaling, delta_px[1]/y_scaling)
+        delta_pos = pow((pow(delta_mm[0], 2) + pow(delta_mm[1], 2)), 0.5)
+        delta_time = frametime - last_time
+        if delta_time > 0:
+            speed = delta_pos / delta_time * 1000
+        else:
+            speed = 0
+
+        speed = int(speed)
+        (text_width, text_height) = cv2.getTextSize(str(speed), font, font_scaling, thickness=font_thickness)[0]
+        cv2.putText(tracker, str(speed), (60 - text_width, 15), font, fontScale=font_scaling, color=(255, 0, 255), thickness=font_thickness)
+        cv2.putText(tracker, 'mm/s', (70, 15), font, fontScale=font_scaling, color=(255, 0, 255), thickness=font_thickness)
+        (text_width, text_height) = cv2.getTextSize(str(int(frametime)), font, font_scaling, thickness=font_thickness)[0]
+        cv2.putText(tracker, str(int(frametime)), (60 - text_width, 30), font, fontScale=font_scaling, color=(255, 0, 255), thickness=font_thickness)
+        cv2.putText(tracker, 'ms', (70, 30), font, fontScale=font_scaling, color=(255, 0, 255), thickness=font_thickness)
+
         last_position = position
+        last_time = frametime
+
+        if show_tracker:
+            cv2.imshow("Tracking image", tracker)
+
         # write data to CSV file
         with open(csv_filename, 'a', newline='') as writefile:
             writer = csv.writer(writefile)
-            writer.writerow((frametime,) + position + delta)
+            writer.writerow((frametime,) + position + delta_px + delta_mm + (delta_pos,) + (speed,))
         writefile.close()
 
         out.write(tracker)
