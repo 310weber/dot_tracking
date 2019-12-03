b5b6dd4528e9bfdf3b09424854423adab8be96bb
devin.kaiser <devin.kaiser@thalmic.com>
2019-12-03 17:05:57 -0500

    Added threading to prevent UI blocking.

--- tracking.py
+++ tracking.py
@@ -1,5 +1,6 @@
 import sys
-from tkinter import Frame, Tk, Scale, HORIZONTAL
+import threading
+from tkinter import Frame, Tk, Scale, HORIZONTAL, Label
 from dot_tracking_class import DotTracking
 
 
@@ -8,27 +9,40 @@ class MainGUI():
         self.DotTracking = DotTracking(fileName)
         frame = Frame(parent)
         frame.pack()
-        self.HSV_min = Scale(frame, from_=0, to=180, orient=HORIZONTAL, command=self.sliderUpdate)
-        self.HSV_min.pack()
-        self.HSV_max = Scale(frame, from_=0, to=180, orient=HORIZONTAL, command=self.sliderUpdate)
-        self.HSV_max.pack()
-        self.DotTracking.Start()
+        self.label1 = Label(frame, text="Hue Min:")
+        #self.label1.pack()
+        self.label1.grid(row=0, column=1)
+        self.HSV_min = Scale(frame, from_=0, to=180, orient=HORIZONTAL, command=self.sliderUpdateMin)
+        self.HSV_min.set(40)
+        self.HSV_min.grid(row=0, column=2)
+        self.label2 = Label(frame, text="Hue Max:")
+        self.label2.grid(row=1, column=1)
+        self.HSV_max = Scale(frame, from_=0, to=180, orient=HORIZONTAL, command=self.sliderUpdateMax)
+        self.HSV_max.set(80)
+        self.HSV_max.grid(row=1, column=2)
+        start_thread = threading.Thread(target=self.DotTracking.Start)
+        start_thread.start()
+        #self.DotTracking.Start()
 
-    def sliderUpdate(self, *args):
-        self.validateSliderBars()
-        self.DotTracking.h_low = self.HSV_min.get()
-        self.DotTracking.h_high = self.HSV_max.get()
-
-    def validateSliderBars(self):
-        if self.HSV_min.get() > self.HSV_max.get():
-            self.HSV_min.set(self.HSV_max.get())
+    def sliderUpdateMax(self, *args):
         if self.HSV_max.get() < self.HSV_min.get():
             self.HSV_max.set(self.HSV_min.get())
+        self.updateHSV()
+
+    def sliderUpdateMin(self, *args):
+        if self.HSV_min.get() > self.HSV_max.get():
+            self.HSV_min.set(self.HSV_max.get())
+        self.updateHSV()
+
+    def updateHSV(self):
+        self.DotTracking.h_low = self.HSV_min.get()
+        self.DotTracking.h_high = self.HSV_max.get()
 
 if __name__ == '__main__':
     parent = Tk()
     GUI = MainGUI(parent, sys.argv[1].strip())
     parent.mainloop()
+    #GUI.DotTracking.Start()
 
 
 
